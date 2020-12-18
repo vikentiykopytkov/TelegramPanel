d3bd6661f4da4053790a7627f17ac6bf95705477
KSoft.Dev <vk@ksoft.online>
2020-12-18 11:58:18 +0300

    'ktsp-2nr'

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import smsapi
 ar = telegram.autoreg.AUTOREGISTRATION(
     API_ID=settings.API_ID,
     API_HASH=settings.API_HASH,
-    session_id='first_test'
+    session_id='debug'
     )
 
-ar.create_accounts()
-print(ar.accounts)
+if __name__ == '__main__':
+    response = ar.create_accounts()
+    if response ['response']:
+        print(ar.accounts)
+        print(ar.get_account(ar.accounts[0]))
+    else:
+        print('Error: ' + response ['error'])
